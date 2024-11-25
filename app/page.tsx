64883985d4358976824c64a0f9eedafb0b0c681e
kolitha.warnakulasooriya@outlook.com <kolitha.warnakulasooriya@outlook.com>
2024-11-25 15:54:25 -0600

    losa

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react"
 
 export default function Page() {
     useEffect(()=>{
-        window.location = 'https://kolithawarnakulasooriya.github.io/maintanance'
+        window.location.assign("https://kolithawarnakulasooriya.github.io/maintanance");
     },[])
     return (
         <h1>Under Costruction</h1>
